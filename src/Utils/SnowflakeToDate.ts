42675082ceccab0557260ded7db6851df8547b5d
Music Maker <joshuanejdl@gmail.com>
2024-09-16 22:51:02 -0500

    Added error message when snowflake is invalid

--- src/Utils/SnowflakeToDate.ts
+++ src/Utils/SnowflakeToDate.ts
@@ -1,4 +1,5 @@
 export default function SnowflakeToDate(snowflake: string): Date {
+	if (typeof snowflake !== 'string') throw new Error(`Invalid snowflake : Expected a string but got ${typeof snowflake}`);
 	return new Date(Number(BigInt(snowflake) >> 22n) + 1420070400000);
 }
 module.exports = exports.default;
