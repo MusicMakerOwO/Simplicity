7cecb58fb5bbfad15855e5689c71c8a4d6359a09
Music Maker <joshuanejdl@gmail.com>
2024-09-17 00:27:26 -0500

    Added guild.create

--- src/Objects/Guild.ts
+++ src/Objects/Guild.ts
@@ -1,48 +1,3 @@
-/*
-id	snowflake	guild id
-name	string	guild name (2-100 characters, excluding trailing and leading whitespace)
-icon	?string	icon hash
-icon_hash?	?string	icon hash, returned when in the template object
-splash	?string	splash hash
-discovery_splash	?string	discovery splash hash; only present for guilds with the "DISCOVERABLE" feature
-owner? *	boolean	true if the user is the owner of the guild
-owner_id	snowflake	id of owner
-permissions? *	string	total permissions for the user in the guild (excludes overwrites and implicit permissions)
-region? **	?string	voice region id for the guild (deprecated)
-afk_channel_id	?snowflake	id of afk channel
-afk_timeout	integer	afk timeout in seconds
-widget_enabled?	boolean	true if the server widget is enabled
-widget_channel_id?	?snowflake	the channel id that the widget will generate an invite to, or null if set to no invite
-verification_level	integer	verification level required for the guild
-default_message_notifications	integer	default message notifications level
-explicit_content_filter	integer	explicit content filter level
-roles	array of role objects	roles in the guild
-emojis	array of emoji objects	custom guild emojis
-features	array of guild feature strings	enabled guild features
-mfa_level	integer	required MFA level for the guild
-application_id	?snowflake	application id of the guild creator if it is bot-created
-system_channel_id	?snowflake	the id of the channel where guild notices such as welcome messages and boost events are posted
-system_channel_flags	integer	system channel flags
-rules_channel_id	?snowflake	the id of the channel where Community guilds can display rules and/or guidelines
-max_presences?	?integer	the maximum number of presences for the guild (null is always returned, apart from the largest of guilds)
-max_members?	integer	the maximum number of members for the guild
-vanity_url_code	?string	the vanity url code for the guild
-description	?string	the description of a guild
-banner	?string	banner hash
-premium_tier	integer	premium tier (Server Boost level)
-premium_subscription_count?	integer	the number of boosts this guild currently has
-preferred_locale	string	the preferred locale of a Community guild; used in server discovery and notices from Discord, and sent in interactions; defaults to "en-US"
-public_updates_channel_id	?snowflake	the id of the channel where admins and moderators of Community guilds receive notices from Discord
-max_video_channel_users?	integer	the maximum amount of users in a video channel
-max_stage_video_channel_users?	integer	the maximum amount of users in a stage video channel
-approximate_member_count?	integer	approximate number of members in this guild, returned from the GET /guilds/<id> and /users/@me/guilds endpoints when with_counts is true
-approximate_presence_count?	integer	approximate number of non-offline members in this guild, returned from the GET /guilds/<id> and /users/@me/guilds endpoints when with_counts is true
-welcome_screen?	welcome screen object	the welcome screen of a Community guild, shown to new members, returned in an Invite's guild object
-nsfw_level	integer	guild NSFW level
-stickers?	array of sticker objects	custom guild stickers
-premium_progress_bar_enabled	boolean	whether the guild has the boost progress bar enabled
-safety_alerts_channel_id	?snowflake	the id of the channel where admins and moderators of Community guilds receive safety alerts from Discord
-*/
 import { APIGuild, APIRole, APIEmoji, APISticker, APIWelcomeScreen, APIChannel } from "../APITypes/Objects";
 import Client from "../Client";
 import SnowflakeToDate from "../Utils/SnowflakeToDate";
@@ -58,6 +13,29 @@ import Role from "./Role";
 import Emoji from "./Emoji";
 import Sticker from "./Sticker";
 import Channel from "./Channel";
+
+import ResolveEndpoint from "../Utils/ResolveEndpoint";
+
+declare type ChannelCreationOptions = {
+	name: string;
+	type: number;
+	topic?: string;
+	bitrate?: number;
+	user_limit?: number;
+	rate_limit_per_user?: number;
+	position?: number;
+	permission_overwrites?: any[];
+	parent_id?: string;
+	nsfw?: boolean;
+	rtc_region?: string;
+	video_quality_mode?: number;
+	default_auto_archive_duration?: number;
+	default_reaction_emoji?: any;
+	available_tags?: any[];
+	default_sort_order?: number;
+	default_forum_layout?: number;
+	default_thread_rate_limit_per_user?: number;
+}
 
 export default class Guild {
 	#client: Client;
@@ -192,6 +170,15 @@ export default class Guild {
 
 	get safetyAlertsChannel() {
 		return this.safety_alerts_channel_id ? this.#client.channels.getSync(this.safety_alerts_channel_id) : null;
+	}
+
+	async createChannel(data: ChannelCreationOptions) : Promise<Channel> {
+		const endpoint = ResolveEndpoint(GuildEndpoints.CREATE_CHANNEL, { guild: this })
+		const response = await this.#client.wsClient?.SendRequest('POST', endpoint, { body: data }) as APIChannel;
+
+		console.log('channel created')
+		
+		return new Channel(this.#client, response)
 	}
 }
 module.exports = exports.default;
