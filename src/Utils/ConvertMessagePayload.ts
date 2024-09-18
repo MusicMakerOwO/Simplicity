09c8546758912680482fe533584d21ed670eaeb4
Music Maker <joshuanejdl@gmail.com>
2024-09-17 22:37:32 -0500

    Removed bugged line that wrapped ActionRows within ActionRows, fix unavailable yet

--- src/Utils/ConvertMessagePayload.ts
+++ src/Utils/ConvertMessagePayload.ts
@@ -5,19 +5,19 @@ export default function ConvertMessagePayload(data: any): MessagePayload {
 	if (!data) throw new Error('Invalid message payload, expected an object or string');
 	if (typeof data === 'string') return ConvertHiddenFlag({ content: data, hidden: false })
 
-	if ("components" in data) {
-		if (data.components.length > 5) throw new Error('Cannot have more than 5 components in a message');
-		// @ts-ignore
-		data.components = data.components.map((c: Object & { type: number }) => c.type === 1 ? c : new ActionRow().addComponent(c));
-	}
-
+	// if ("components" in data) {
+	// 	if (data.components.length > 5) throw new Error('Cannot have more than 5 components in a message');
+	// 	// @ts-ignore
+	// 	data.components = data.components.map((c: Object & { type: number }) => c.type === 1 ? c : new ActionRow().addComponent(c));
+	// }
+	
 	if ("type" in data) {
 		const payload: MessagePayload = { content: data.content, hidden: Boolean(data.hidden), components: [] };
 		// Action row, forgot to wrap in message.components
 		if (data.type === 1) payload.components = [ data ];
 		// Button or select menu, forgot to wrap in action row
 		if (data.type === 2 || data.type === 3) payload.components = [ new ActionRow().addComponent(data) ];
-
+		
 		return ConvertHiddenFlag(payload);
 	}
 
