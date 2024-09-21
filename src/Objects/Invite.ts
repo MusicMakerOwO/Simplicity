d0ebef8964da87e89d7bd251d5004bd745ac8b5b
Music Maker <joshuanejdl@gmail.com>
2024-09-21 12:21:48 -0500

    Added Invite.ts object, not hooked up to cache yet

---
+++ src/Objects/Invite.ts
@@ -0,0 +1,71 @@
+import Client from "../Client";
+import { APIInvite } from "../APITypes/Objects";
+
+import User from "./User";
+
+/*
+export declare type APIInvite = {
+	channel_id: string;
+	code: string;
+	created_at: string;
+	guild_id?: string;
+	inviter?: APIUser;
+	max_age: number;
+	max_uses: number;
+	target_type?: number;
+	target_user?: APIUser;
+	target_application?: any;
+	temporary: boolean;
+	uses: number;
+}
+*/
+
+export default class Invite {
+	#client: Client;
+
+	public readonly code: string;
+	public readonly channelID: string;
+	public readonly guildID: string | null;
+	public readonly inviter: User | null;
+	public readonly maxAge: number;
+	public readonly maxUses: number;
+	public readonly targetUser: User | null;
+	public readonly targetApplication: any;
+	public readonly temporary: boolean;
+	public readonly uses: number;
+
+	public readonly created_at: Date;
+	public readonly expires_at: Date | null;
+
+	constructor(client: Client, data: APIInvite) {
+		this.#client = client;
+
+		this.code = data.code;
+		this.channelID = data.channel_id;
+		this.guildID = data.guild_id ?? null;
+		this.inviter = data.inviter ? new User(client, data.inviter) : null;
+		this.maxAge = data.max_age; // 0 if infinite
+		this.maxUses = data.max_uses;
+		this.targetUser = data.target_user ? new User(client, data.target_user) : null;
+		this.targetApplication = data.target_application;
+		this.temporary = data.temporary;
+		this.uses = data.uses;
+
+		this.created_at = new Date(data.created_at);
+		this.expires_at = data.max_age
+			? new Date(this.created_at.getTime() + data.max_age * 1000)
+			: null;
+	}
+
+	getURL(): string {
+		return `https://discord.gg/${this.code}`;
+	}
+
+	toString(): string {
+		return this.getURL();
+	}
+
+	inspect(): string {
+		return `<Invite ${this.code}>`;
+	}
+}
