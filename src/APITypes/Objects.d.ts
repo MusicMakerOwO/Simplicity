a84193f99d49c55077c348252b4c1fa89de915a9
Music Maker <joshuanejdl@gmail.com>
2024-09-22 12:35:05 -0500

    Added voiceStateUpdate and voiceServerUpdate types

--- src/APITypes/Objects.d.ts
+++ src/APITypes/Objects.d.ts
@@ -644,3 +644,35 @@ export declare type APIInvite = {
 	temporary: boolean;
 	uses: number;
 }
+
+
+/*
+guild_id?	snowflake	the guild id this voice state is for
+channel_id	?snowflake	the channel id this user is connected to
+user_id	snowflake	the user id this voice state is for
+member?	guild member object	the guild member this voice state is for
+session_id	string	the session id for this voice state
+deaf	boolean	whether this user is deafened by the server
+mute	boolean	whether this user is muted by the server
+self_deaf	boolean	whether this user is locally deafened
+self_mute	boolean	whether this user is locally muted
+self_stream?	boolean	whether this user is streaming using "Go Live"
+self_video	boolean	whether this user's camera is enabled
+suppress	boolean	whether this user's permission to speak is denied
+request_to_speak_timestamp	?ISO8601 timestamp	the time at which the user requested to speak
+*/
+export declare type APIVoiceStateUpdate = {
+	guild_id?: string;
+	channel_id?: string;
+	user_id: string;
+	member?: APIMember;
+	session_id: string;
+	deaf: boolean;
+	mute: boolean;
+	self_deaf: boolean;
+	self_mute: boolean;
+	self_stream?: boolean;
+	self_video: boolean;
+	suppress: boolean;
+	request_to_speak_timestamp?: string;
+};
