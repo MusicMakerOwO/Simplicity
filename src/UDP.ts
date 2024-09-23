82eb686c6f517e29dc1c686d7305a34ba55100f5
Music Maker <joshuanejdl@gmail.com>
2024-09-23 00:55:20 -0500

    Added UDP connection class

---
+++ src/UDP.ts
@@ -0,0 +1,43 @@
+import dgram from 'node:dgram';
+import Events from './Events';
+
+export default class UDP extends Events {
+	#client: dgram.Socket;
+	#port: number;
+	#address: string;
+
+	constructor(port: number, address: string) {
+		super();
+
+		this.#client = dgram.createSocket('udp4');
+		this.#port = port;
+		this.#address = address;
+
+		this.#client.on('message', this.emit.bind(this, 'message'));
+
+		this.#client.on('close', () => {
+			this.emit('close');
+		});
+
+		this.#client.on('error', (err) => {
+			this.emit('error', err);
+			this.#client.close();
+		});
+	}
+
+	send(data: string | Buffer) {
+		const bufferData = typeof data === 'string' ? Buffer.from(data) : data;
+		this.#client.send(bufferData, this.#port, this.#address, (err) => {
+			if (err) {
+				this.emit('error', err);
+			} else {
+				this.emit('send', data);
+			}
+		});
+	}
+
+	close() {
+		if (this.#client) this.#client.close();
+	}
+}
+module.exports = exports.default;
