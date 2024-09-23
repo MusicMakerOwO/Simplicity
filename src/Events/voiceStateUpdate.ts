128146283cb4198164aaa3f6ee35befb00702a2e
Music Maker <joshuanejdl@gmail.com>
2024-09-22 22:47:11 -0500

    Connected voiceStateUpdate event to add session_id to VCPlayer

--- src/Events/voiceStateUpdate.ts
+++ src/Events/voiceStateUpdate.ts
@@ -11,6 +11,11 @@ export default {
 			client.emit(APIEvents.VOICE_STATE_UPDATE, data);
 			return;
 		}
+
+		const player = client.vcClient.connections.get(data.guild_id);
+		if (!player) return;
+
+		player.sessionID = data.session_id;
 	}
 }
 module.exports = exports.default;
