fbce61d66243467c58a90e29d1e409d7595eb185
Music Maker <joshuanejdl@gmail.com>
2024-10-06 00:39:52 -0500

    Typings for VoicePlugins, more to come

---
+++ src/Voice/types.d.ts
@@ -0,0 +1,9 @@
+export interface VoicePlugin {
+	constructor(): void;
+	init(bitrate: number, bitdepth: number, samples: number): void;
+	process(audio: Float32Array, options?: unknown): Promise<Float32Array>;
+}
+
+export interface VoicePluginConstructor {
+	new (): VoicePlugin;
+}
