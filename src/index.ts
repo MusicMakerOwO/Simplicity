8d99558261412c46aa562b877d479f242b86675d
Music Maker <joshuanejdl@gmail.com>
2024-10-29 02:46:42 -0500

    Added an index.js for centeralized access, more to come

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+exports.Client = require('./Client');
+exports.WSClient = require('./WSClient');
+exports.VCClient = require('./VCClient');
+exports.UDPClient = require('./UDP');
+exports.Websocket = require('./Websocket');
+exports.EventListener = require('./EventListener');
+exports.EventDispatcher = require('./EventDispatcher');
+
+
+// export * as Client from './Client';
+// export * as WSClient from './WSClient';
+// export * as VCClient from './VCClient';
+// export * as UDPClient from './UDP';
+// export * as Websocket from './Websocket';
+// export * as EventListener from './EventListener';
+// export * as EventDispatcher from './EventDispatcher';
+
+// module.exports = {
+// 	Client: require('./Client'),
+// 	WSClient: require('./WSClient'),
+// 	VCClient: require('./VCClient'),
+
+// 	UDPClient: require('./UDP'),
+// 	Websocket: require('./Websocket'),
+
+// 	EventListener: require('./EventListener'),
+// 	EventDispatcher: require('./EventDispatcher'),
+// };
