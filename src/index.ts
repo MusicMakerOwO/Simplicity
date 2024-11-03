066d61a7c5734341045baf921432c04a5bdf0636
Music Maker <joshuanejdl@gmail.com>
2024-11-03 01:22:16 -0600

    fix: Switched index to import/export for typings

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,36 @@
 require('./PluginManager'); // runs automatically
 
+import Client from './Client';
+import WSClient from './WSClient';
+import VCClient from './VCClient';
+import UDP from './UDP';
+import Websocket from './Websocket';
+import EventListener from './EventListener';
+import EventDispatcher from './EventDispatcher';
 
-exports.Client = require('./Client');
-exports.WSClient = require('./WSClient');
-exports.VCClient = require('./VCClient');
-exports.UDPClient = require('./UDP');
-exports.Websocket = require('./Websocket');
-exports.EventListener = require('./EventListener');
-exports.EventDispatcher = require('./EventDispatcher');
+export {
+	Client,
+	WSClient,
+	VCClient,
+	UDP,
+	Websocket,
+	EventListener,
+	EventDispatcher,
+};
+
+// exports.Client = require('./Client');
+// exports.WSClient = require('./WSClient');
+// exports.VCClient = require('./VCClient');
+// exports.UDP = require('./UDP');
+// exports.Websocket = require('./Websocket');
+// exports.EventListener = require('./EventListener');
+// exports.EventDispatcher = require('./EventDispatcher');
 
 
 // export * as Client from './Client';
 // export * as WSClient from './WSClient';
 // export * as VCClient from './VCClient';
-// export * as UDPClient from './UDP';
+// export * as UDP from './UDP';
 // export * as Websocket from './Websocket';
 // export * as EventListener from './EventListener';
 // export * as EventDispatcher from './EventDispatcher';
@@ -23,7 +40,7 @@ exports.EventDispatcher = require('./EventDispatcher');
 // 	WSClient: require('./WSClient'),
 // 	VCClient: require('./VCClient'),
 
-// 	UDPClient: require('./UDP'),
+// 	UDP: require('./UDP'),
 // 	Websocket: require('./Websocket'),
 
 // 	EventListener: require('./EventListener'),
