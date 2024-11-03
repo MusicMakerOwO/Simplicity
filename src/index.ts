ba40e3e58056983e13a6ea977e8b6b982e19d797
Music Maker <joshuanejdl@gmail.com>
2024-11-03 01:25:56 -0600

    fix: Cleaned up comments in index

--- src/index.ts
+++ src/index.ts
@@ -17,32 +17,3 @@ export {
 	EventListener,
 	EventDispatcher,
 };
-
-// exports.Client = require('./Client');
-// exports.WSClient = require('./WSClient');
-// exports.VCClient = require('./VCClient');
-// exports.UDP = require('./UDP');
-// exports.Websocket = require('./Websocket');
-// exports.EventListener = require('./EventListener');
-// exports.EventDispatcher = require('./EventDispatcher');
-
-
-// export * as Client from './Client';
-// export * as WSClient from './WSClient';
-// export * as VCClient from './VCClient';
-// export * as UDP from './UDP';
-// export * as Websocket from './Websocket';
-// export * as EventListener from './EventListener';
-// export * as EventDispatcher from './EventDispatcher';
-
-// module.exports = {
-// 	Client: require('./Client'),
-// 	WSClient: require('./WSClient'),
-// 	VCClient: require('./VCClient'),
-
-// 	UDP: require('./UDP'),
-// 	Websocket: require('./Websocket'),
-
-// 	EventListener: require('./EventListener'),
-// 	EventDispatcher: require('./EventDispatcher'),
-// };
