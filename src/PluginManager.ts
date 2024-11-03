27c34a9ee21519b583a03cb4429beaa3f5f5cc2e
Music Maker <joshuanejdl@gmail.com>
2024-11-02 23:01:42 -0500

    feat: Added basic plugin validation - More checks needed

--- src/PluginManager.ts
+++ src/PluginManager.ts
@@ -1,7 +1,17 @@
 const Module = require('module');
 const fs = require('fs');
 
+const PrimaryPackage = require(`../package.json`);
+
 const resolutionTable = new Map<string, string>();
+
+interface PluginPackage {
+	name: string;
+	version: string;
+	description: string;
+	author: string;
+	supported_versions: string[];
+}
 
 // This a private method and almost completely undocumented
 // This is a dependency of require(), it checks the cache and sends the code to the compiler if needed
@@ -35,10 +45,33 @@ function RecursiveReadFolder(folder: string) {
 	return files;
 }
 
+const ConflictingPlugins = new Map<string, string>();
+
 const plugins = fs.readdirSync(`${__dirname}/../Plugins`, { withFileTypes: true });
 for (const plugin of plugins) {
 	if (!plugin.isDirectory()) continue;
 	const pluginPath = `${__dirname}/../Plugins/${plugin.name}`;
+
+	try {
+		var pluginPackage = require(`${pluginPath}/package.json`) as PluginPackage;
+	} catch (e) {
+		console.error(`Failed to load "${plugin.name}" - Missing or malformed package.json file`);
+		continue;
+	}
+
+	const error = VerifyPackageIntegrity(pluginPackage);
+	if (error) {
+		console.error(`Failed to load "${plugin.name}" - ${error}`);
+		continue;
+	}
+
+	if (!pluginPackage.supported_versions.includes(PrimaryPackage.version)) {
+		console.error(`[${plugin.name}] Plugin "${pluginPackage.name}" is not supported by this version of Simplicity`);
+		console.error(`[${plugin.name}] Plugin will be allowed to load but may not function correctly`);
+		console.error(`[${plugin.name}] Simplicity version: ${PrimaryPackage.version}`);
+		console.error(`[${plugin.name}] Supported versions: ${pluginPackage.supported_versions.join(', ')}`);
+	}
+
 	const pluginFiles = RecursiveReadFolder(pluginPath);
 	for (const file of pluginFiles) {
 		const relativePath = file.replace(`${__dirname}/../Plugins/${plugin.name}/`, '');
@@ -48,5 +81,15 @@ for (const plugin of plugins) {
 		} catch (e) {
 			// ignore, file doesn't exist
 		}
+	}
+}
+
+function VerifyPackageIntegrity(pluginPackage: PluginPackage) {
+	if (typeof pluginPackage.name !== 'string') return 'Invalid name, must be a string';
+	if (typeof pluginPackage.version !== 'string') return 'Invalid version, must be a string';
+	if (typeof pluginPackage.author !== 'string') return 'Invalid author, must be a string';
+	if (!Array.isArray(pluginPackage.supported_versions)) return 'Invalid supported_versions, must be an array';
+	for (let i = 0; i < pluginPackage.supported_versions.length; i++) {
+		if (typeof pluginPackage.supported_versions[i] !== 'string') return 'Invalid supported_versions, must be an array of strings';
 	}
 }
