c1aea3b5943e338d4380b79125217092afba13b5
Music Maker <joshuanejdl@gmail.com>
2024-11-04 10:29:25 -0600

    feat: Added a bunch of exports to the index, more to come

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 require('./PluginManager'); // runs automatically
 
+// Primary
 import Client from './Client';
 import WSClient from './WSClient';
 import VCClient from './VCClient';
@@ -8,7 +9,69 @@ import Websocket from './Websocket';
 import EventListener from './EventListener';
 import EventDispatcher from './EventDispatcher';
 
+// Objects
+import Channel from './Objects/Channel';
+import Collector from './Objects/Collector';
+import Emoji from './Objects/Emoji';
+import Guild from './Objects/Guild';
+import Interaction from './Objects/Interaction';
+import Invite from './Objects/Invite';
+import Member from './Objects/Member';
+import Message from './Objects/Message';
+import Role from './Objects/Role';
+import Sticker from './Objects/Sticker';
+import User from './Objects/User';
+
+// Enums
+import ButtonStyles from './Enums/ButtonStyles';
+import ChannelFlags from './Enums/ChannelFlags';
+import CommandOptionTypes from './Enums/CommandOptionTypes';
+import ComponentTypes from './Enums/ComponentTypes';
+import { WSEvents, ClientEvents } from './Enums/Events';
+import Intents from './Enums/Intents';
+import InteractionCallbackType from './Enums/InteractionCallbackType';
+import InteractionTypes from './Enums/InteractionType';
+import NitroSubscriptions from './Enums/NitroSubscriptions';
+import { Status, Pressence } from './Enums/Status';
+import UserFlags from './Enums/UserFlags';
+
+// Data structures
+import BitField from './DataStructures/BitField';
+import ClientCache from './DataStructures/ClientCache';
+import LRUCache from './DataStructures/LRUCache';
+import SlidingCache from './DataStructures/SlidingCache';
+
+// Builders (Components)
+import Embed from './Builders/Embed';
+import Button from './Builders/Components/Button';
+import ActionRow from './Builders/Components/ActionRow';
+import Modal from './Builders/Components/Modal';
+import ModalQuestion from './Builders/Components/ModalQuestion';
+import PremiumButton from './Builders/Components/PremiumButton';
+import SelectMenu from './Builders/Components/SelectMenu';
+import SelectMenuOption from './Builders/Components/SelectMenuOption';
+
+// Builders (Commands)
+import ApplyOptionMethods from './Builders/Commands/ApplyOptionMethods';
+import Command from './Builders/Commands/Command';
+import SlashCommand from './Builders/Commands/SlashCommand';
+import SubCommand from './Builders/Commands/SubCommand';
+import SubCommandGroup from './Builders/Commands/SubCommandGroup';
+
+// Builders (Command Options)
+import BaseOption from './Builders/CommandOptions/Option';
+import AttachmentOption from './Builders/CommandOptions/AttachmentOption';
+import BooleanOption from './Builders/CommandOptions/BooleanOption';
+import ChannelOption from './Builders/CommandOptions/ChannelOption';
+import IntegerOption from './Builders/CommandOptions/IntegerOption';
+import MentionableOption from './Builders/CommandOptions/MentionableOption';
+import NumberOption from './Builders/CommandOptions/NumberOption';
+import RoleOption from './Builders/CommandOptions/RoleOption';
+import StringOption from './Builders/CommandOptions/StringOption';
+import UserOption from './Builders/CommandOptions/UserOption';
+
 export {
+	// Primary
 	Client,
 	WSClient,
 	VCClient,
@@ -16,4 +79,67 @@ export {
 	Websocket,
 	EventListener,
 	EventDispatcher,
+
+	// Objects
+	Channel,
+	Collector,
+	Emoji,
+	Guild,
+	Interaction,
+	Invite,
+	Member,
+	Message,
+	Role,
+	Sticker,
+	User,
+
+	// Enums
+	ButtonStyles,
+	ChannelFlags,
+	CommandOptionTypes,
+	ComponentTypes,
+	WSEvents,
+	ClientEvents,
+	Intents,
+	InteractionCallbackType,
+	InteractionTypes,
+	NitroSubscriptions,
+	Status,
+	Pressence,
+	UserFlags,
+
+	// Data structures
+	BitField,
+	ClientCache,
+	LRUCache,
+	SlidingCache,
+
+	// Builders (Components)
+	Embed,
+	Button,
+	ActionRow,
+	Modal,
+	ModalQuestion,
+	PremiumButton,
+	SelectMenu,
+	SelectMenuOption,
+	
+	// Builders (Commands)
+	ApplyOptionMethods,
+	Command,
+	SlashCommand,
+	SubCommand,
+	SubCommandGroup,
+	
+	// Builders (Command Options)
+	BaseOption,
+	AttachmentOption,
+	BooleanOption,
+	ChannelOption,
+	IntegerOption,
+	MentionableOption,
+	NumberOption,
+	RoleOption,
+	StringOption,
+	UserOption
 };
